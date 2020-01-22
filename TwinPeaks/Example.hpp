c54c224e5315bb1b68da5b867384f2c3fa04d74a
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-01-22 20:25:14 +1300

    Scalars

--- TwinPeaks/Example.hpp
+++ TwinPeaks/Example.hpp
@@ -2,6 +2,7 @@
 #define TwinPeaks_Example_hpp
 
 #include "RNG.hpp"
+#include <tuple>
 
 namespace TwinPeaks
 {
@@ -16,9 +17,13 @@ class Example
         std::vector<double> xs;
 
     public:
+
         // Constructor must generate from prior
         Example();
         void from_prior(RNG& rng);
+
+        // Return scalars
+        std::tuple<double, double> get_scalars() const;
 
 };
 
@@ -34,6 +39,24 @@ void Example::from_prior(RNG& rng)
 {
     for(int i=0; i<N; ++i)
         xs[i] = rng.rand();
+}
+
+std::tuple<double, double> Example::get_scalars() const
+{
+    double f1 = 0.0;
+    double f2 = 0.0;
+
+    // Parameters of the two peaks
+    static constexpr double w = 0.01;
+    static constexpr double tau = 1.0/(w*w);
+
+    for(double x: xs)
+    {
+        f1 += -0.5*tau*pow(x - 0.45, 2);
+        f2 += -0.5*tau*pow(x - 0.55, 2);
+    }
+
+    return {f1, f2};
 }
 
 } // namespace
