4b60a886b99b456d3b3f84c865a7fa72d8f0f99b
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-01-23 13:29:17 +1300

    run options type

--- TwinPeaks/RunOptions.hpp
+++ TwinPeaks/RunOptions.hpp
@@ -1,13 +1,11 @@
 #ifndef TwinPeaks_RunOptions_hpp
 #define TwinPeaks_RunOptions_hpp
 
+#include <fstream>
+#include <iomanip>
+
 namespace TwinPeaks
 {
-
-//// Forward declare
-//template<typename T>
-//class Sampler<T>;
-
 
 // This should be fairly self explanatory
 class RunOptions
@@ -22,17 +20,46 @@ class RunOptions
         // Just specify depth, use defaults for the others
         RunOptions(double _depth);
 
-
         // Specify everything
         RunOptions(int _num_particles, int _mcmc_steps, double _depth);
 
-//        // Friend
-//        template<typename T>
-//        friend class Sampler<T>;
+        // Output to YAML file
+        void save() const;
+
+        // Friend
+        template <typename T>
+        friend class Sampler;
 
 };
 
 /* IMPLEMENTATIONS FOLLOW */
+
+RunOptions::RunOptions(double _depth)
+:num_particles(1000)
+,mcmc_steps(1000)
+,depth(_depth)
+{
+
+}
+
+RunOptions::RunOptions(int _num_particles, int _mcmc_steps, double _depth)
+:num_particles(_num_particles)
+,mcmc_steps(_mcmc_steps)
+,depth(_depth)
+{
+
+}
+
+void RunOptions::save() const
+{
+    std::fstream fout("output/run_options.yaml", std::ios::out);
+    fout << std::setprecision(12);
+    fout << "---\n";
+    fout << "num_particles: " << num_particles << '\n';
+    fout << "mcmc_steps: " << num_particles << '\n';
+    fout << "depth: " << depth << std::endl;
+    fout.close();
+}
 
 } // namespace
 
