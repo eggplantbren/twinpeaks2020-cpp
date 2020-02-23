a325914add97cc73906336f80a500b0fea09f46c
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-02-23 21:20:02 +1300

    not quite working perfectly

--- restrict.py
+++ restrict.py
@@ -2,14 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as rng
 
-N = 5
-alpha = 0
+N = 99
+alpha = -0.99999
 beta = np.sqrt(1.0 - alpha**2)
 
-reps = 1
+reps = 1000000
 good = []
 for rep in range(reps):
-
 
     # Particle stuff
     xs = rng.randn(N)
@@ -21,52 +20,59 @@ for rep in range(reps):
     y_ranks = np.empty(N)
     y_ranks[y_indices] = np.arange(0, N)
 
-    # Existing particles have xx in {0.5, 1.5, ..., N-0.5} (length N)
-    xx = x_ranks + 0.5
-    yy = y_ranks + 0.5
+    # Existing particles have xx in {0.5, 1.5, ..., N-0.5}/N (length N)
+    xr, yr = x_ranks + 0.5, y_ranks + 0.5
+    X = 1.0 - xr/N
+    Y = 1.0 - yr/N
 
     # Target scalar at particles
-    qq = N/((N-xx)*(N-yy) + 1)
+    Q = 1.0/(X*Y + 1.0)
 
     # Worst particle
-    worst = np.nonzero(qq == qq.min())[0]
-    thresh = qq[worst][0]
+    worst = np.nonzero(Q == Q.min())[0]
+    thresh = Q[worst][0]
 
     # Grid - new particles would land here
-    xx_grid, yy_grid = np.meshgrid(np.arange(N+1), np.arange(N+1))
-    qq_grid = N/((N - xx_grid)*(N - yy_grid) + 1)
+    # New particles have xr in {0, 1, ..., N}/N (length N)
+    xr_grid, yr_grid = np.meshgrid(np.arange(N+1), np.arange(N+1))
+    X_grid = 1.0 - xr_grid/N
+    Y_grid = 1.0 - yr_grid/N
+    Q_grid = 1.0/(X_grid*Y_grid + 1.0)
 
-    plt.figure(figsize=(8, 8))
-    plt.plot(xx[qq > thresh],  yy[qq > thresh],   "ys", markersize=10)
-    plt.plot(xx[qq == thresh], yy[qq == thresh], color="y", marker="X",
-                linestyle="None", markersize=10)
+#    plt.close("all")
+#    plt.figure(figsize=(8, 8))
+#    plt.plot(xr[Q > thresh], yr[Q > thresh], "ys", markersize=10)
+#    plt.plot(xr[Q == thresh], yr[Q == thresh], color="y", marker="X",
+#                linestyle="None", markersize=10)
 
-    plt.plot(xx_grid[qq_grid > thresh].flatten(),
-             yy_grid[qq_grid > thresh].flatten(), color="lightgreen",
-                    linestyle="None", marker="*")
-    plt.plot(xx_grid[qq_grid < thresh].flatten(),
-             yy_grid[qq_grid < thresh].flatten(), "r*")
-    plt.plot(xx_grid[qq_grid == thresh].flatten(),
-             yy_grid[qq_grid == thresh].flatten(), "y*")
-
-
-#    plt.plot(Xgrid.flatten(), Ygrid.flatten(), "o", color="gray")
-#    plt.imshow((Qgrid > thresh).astype("int64"), origin="lower",
-#               cmap="coolwarm_r")
-    plt.show()
+#    plt.plot(xr_grid[Q_grid > thresh].flatten(),
+#             yr_grid[Q_grid > thresh].flatten(), color="lightgreen",
+#                    linestyle="None", marker="*")
+#    plt.plot(xr_grid[Q_grid < thresh].flatten(),
+#             yr_grid[Q_grid < thresh].flatten(), color="red",
+#                    linestyle="None", marker="*")
+#    plt.plot(xr_grid[Q_grid == thresh].flatten(),
+#             yr_grid[Q_grid == thresh].flatten(), color="yellow",
+#                    linestyle="None", marker="*")
+#    plt.show()
 
 
 
-#    # Generate a new particle
-#    xnew = rng.randn()
-#    ynew = alpha*xs + beta*rng.randn()
-#    Xnew = N - (np.sum(xs < xnew) - 0.5)
-#    Ynew = N - (np.sum(ys < ynew) - 0.5)
-#    Qnew = 1.0/(Xnew*Ynew)
+    # Generate a new particle
+    x_new = rng.randn()
+    y_new = alpha*x_new + beta*rng.randn()
+    xr_new = np.sum(xs < x_new)
+    yr_new = np.sum(ys < y_new)
+    X_new = 1.0 - xr_new/N
+    Y_new = 1.0 - yr_new/N
+    Q_new = 1.0/(X_new*Y_new + 1.0)
 
-#    good.append(Qnew >= thresh)
-#    if (rep+1)%100 == 0:
-#        print(rep+1, np.mean(good), np.std(good)/np.sqrt(rep+1))
+    if Q_new == thresh:
+        print("D'oh")
+
+    good.append(Q_new > thresh)
+    if (rep+1)%100 == 0:
+        print(rep+1, np.mean(good), np.std(good)/np.sqrt(rep+1))
 
 
 
